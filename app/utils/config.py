4eacf1c1db5c5c99117273f519eff9f9f8c1ed1b
Aravindreddy2 <140465971+Aravindreddy2@users.noreply.github.com>
2026-01-04 21:24:58 +0530

    config.py file

---
+++ app/utils/config.py
@@ -0,0 +1,10 @@
+# app/utils/config.py
+
+LLAMA_API_URL = "https://api.groq.com/openai/v1/chat/completions"
+LLAMA_API_KEY = "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"  
+EMBEDDING_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
+VECTOR_STORE_PATH = "./data/embeddings"
+# app/utils/config.py
+ALERT_EMAIL = "aravindreddyashireddy@gmail.com"
+ALERT_EMAIL_PASSWORD = "wtrs llcd exjw mpio"  # Gmail app password (not normal password)
+
