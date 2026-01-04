ee6b2cbfaf5dc128db41d3f42beb46f1ee800ffd
Aravindreddy2 <140465971+Aravindreddy2@users.noreply.github.com>
2026-01-04 21:34:57 +0530

    config.py file update

--- app/utils/config.py
+++ app/utils/config.py
@@ -1,10 +1,15 @@
 # app/utils/config.py
+# app/utils/config.py
+import streamlit as st
 
-LLAMA_API_URL = "https://api.groq.com/openai/v1/chat/completions"
-LLAMA_API_KEY = "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"  
+LLAMA_API_KEY = st.secrets["LLAMA_API_KEY"]
+LLAMA_API_URL = st.secrets["LLAMA_API_URL"]
+
+
+  
 EMBEDDING_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
 VECTOR_STORE_PATH = "./data/embeddings"
 # app/utils/config.py
 ALERT_EMAIL = "aravindreddyashireddy@gmail.com"
-ALERT_EMAIL_PASSWORD = "wtrs llcd exjw mpio"  # Gmail app password (not normal password)
+ALERT_EMAIL_PASSWORD = "wtrs llcd exjw mpio" 
 
